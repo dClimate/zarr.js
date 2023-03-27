45846adf823cebd10d0bde91b6fc7fdfb2ee366f
TheGreatAlgo <37487508+TheGreatAlgo@users.noreply.github.com>
2023-03-27 13:37:44 +0200

    fix: hamt

--- src/storage/ipfsStore.ts
+++ src/storage/ipfsStore.ts
@@ -1,7 +1,11 @@
 import { AsyncStore } from "./types";
 import { KeyError } from "../errors";
+import { load } from 'ipld-hashmap';
+import { sha256 as blockHasher } from 'multiformats/hashes/sha2';
+import * as blockCodec from '@ipld/dag-cbor'; // encode blocks using the DAG-CBOR format
 import { concat as uint8ArrayConcat } from "uint8arrays/concat";
 import { Zlib, Blosc } from "numcodecs";
+import { CID } from 'multiformats/cid';
 import { addCodec } from "../zarr-core";
 
 import all from "it-all";
@@ -18,10 +22,26 @@ export class IPFSSTORE<CID = any, IPFSCLIENT = any>
     public cid: CID;
     public directory: any;
     public ipfsClient: any;
+    public loader: any;
+    public hamt: boolean;
 
-    constructor(cid: CID, ipfsClient: IPFSCLIENT) {
+    constructor(cid: CID, ipfsClient: any) {
         this.cid = cid;
+        this.hamt = false;
         this.ipfsClient = ipfsClient;
+        this.loader = {
+            async get(cid: CID) {
+                const bytes = await ipfsClient.block.get(cid, {
+                    codec: "dag-cbor",
+                });
+                return bytes;
+            },
+            // For our purposes of reading the HashMap we don't need to implement put
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            async put(cid: CID, bytes: ArrayBuffer) {
+                return null;
+            },
+        };
     }
 
     keys(): Promise<string[]> {
@@ -41,7 +61,6 @@ export class IPFSSTORE<CID = any, IPFSCLIENT = any>
                 throw new Error("Zarr does not exist at CID");
             } else {
                 let jsonKey = "";
-                let combinedTree = {};
                 // Find the location of the data being addressed. This is done by checking for an area with more than one dimension
                 // eslint-disable-next-line @typescript-eslint/no-unused-vars
                 for (const [key, keyValue] of Object.entries(value.value[".zmetadata"].metadata)) {
@@ -52,60 +71,49 @@ export class IPFSSTORE<CID = any, IPFSCLIENT = any>
                     // eslint-disable-next-line no-empty
                     } catch (error) {}
                 }
-                // To rebuild the tree we assume the data is found 
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                for (const [secondKey, secondKeyValue] of Object.entries(
-                    value.value[jsonKey],
-                )) {
-                    // If a tree exists we denominate the start of the object with a "/"
-                    if (secondKey.includes("/")) {
-                        const newCID = value.value[jsonKey][secondKey];
-                        const branch = await this.ipfsClient.dag.get(
-                            newCID,
-                        );
-                        combinedTree = Object.assign(
-                            combinedTree,
-                            branch.value,
-                        );
-                    // If an object does not have it and is not the ".zarray" or ".zattrs" then no tree exists
-                    } else if (secondKey !== ".zarray" && secondKey !== ".zattrs") {
-                        // assign to directory for later returns
-                        this.directory = value.value[jsonKey];
-                        // Ensure a codec is loaded
-                        try {
-                            if (value.value[".zmetadata"].metadata[`${jsonKey}/.zarray`].compressor.id === "zlib") {
-                                addCodec(Zlib.codecId, () => Zlib);
-                            }
-                            if (value.value[".zmetadata"].metadata[`${jsonKey}/.zarray`].compressor.id === "blosc") {
-                                addCodec(Zlib.codecId, () => Blosc);
-                            } 
-                        // eslint-disable-next-line no-empty
-                        } catch (error) {}
-                        return value.value[".zmetadata"].metadata[`${jsonKey}/.zarray`];
-                    }
+            // now check if using hamt
+            if (value.value.hamt) {
+                this.hamt = true;
+                // if there is a hamt, load it
+                const hamtOptions: any = { blockHasher, blockCodec };
+                const hamt = await load(this.loader, CID.parse(value.value.hamt["/"]), hamtOptions);
+                this.directory = hamt;
+            } else {
+                this.hamt = false;
+                this.directory = value.value[jsonKey];
+            }
+               
+            // Ensure a codec is loaded
+            try {
+                if (value.value[".zmetadata"].metadata[`${jsonKey}/.zarray`].compressor.id === "zlib") {
+                    addCodec(Zlib.codecId, () => Zlib);
                 }
-                // after the tree has been rebuilt, assign to the directory for parsing later
-                this.directory = combinedTree;         
-                // Ensure a codec is loaded
-                try {
-                    if (value.value[".zmetadata"].metadata[`${jsonKey}/.zarray`].compressor.id === "zlib") {
-                        addCodec(Zlib.codecId, () => Zlib);
-                    }
-                    if (value.value[".zmetadata"].metadata[`${jsonKey}/.zarray`].compressor.id === "blosc") {
-                        addCodec(Zlib.codecId, () => Blosc);
-                    } 
-                // eslint-disable-next-line no-empty
-                } catch (error) {}
-        
+                if (value.value[".zmetadata"].metadata[`${jsonKey}/.zarray`].compressor.id === "blosc") {
+                    addCodec(Zlib.codecId, () => Blosc);
+                } 
+            // eslint-disable-next-line no-empty
+            } catch (error) {}
                 return value.value[".zmetadata"].metadata[`${jsonKey}/.zarray`];
             }
         } else {
+            // example is tp/0.3.2
+            if (this.hamt) {
+                const location = await this.directory.get(item);
+                if (location) {
+                    const value = uint8ArrayConcat(
+                        await all(this.ipfsClient.cat(location)),
+                    );
+                    return value.buffer;
+                } else {
+                    throw new KeyError(item);
+                }
+            }
             if (this.directory[item]) {
                 const value = uint8ArrayConcat(
                     await all(this.ipfsClient.cat(this.directory[item])),
                 );
                 return value.buffer;
-            } else {
+            }  else {
                 throw new KeyError(item);
             }
         }
